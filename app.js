6e304ce5ecc52d7d04d9da3885a4996049cf1f39
zrajesh <rajesh.bhattarai@beatoapp.com>
2023-12-18 18:24:17 +0530

    video streaming 2.0

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const axios = require("axios");
 const app = express();
 
 const PORT = 3000;
@@ -8,36 +9,51 @@ const PORT = 3000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/index.html");
-    // res.send("hello");
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/video", (req, res) => {
-    let range = req.headers.range;
-    if (!range) {
-        // range = 'bytes=0-'
-        res.status(400).send("Requires range header");
+app.get("/video", async (req, res) => {
+    try {
+        let range = req.headers.range;
+        if (!range) {
+            res.status(400).send("Requires range header");
+            return;
+        }
+
+        const videoUrl = "https://beato-storage.s3.ap-south-1.amazonaws.com/videos/test.mp4"; // Replace with your AWS video URL
+        const response = await axios.head(videoUrl);
+        const videoSize = response.headers['content-length'];
+
+        const CHUNK_SIZE = 10**6; // 1 MB
+        const start = Number(range.replace(/\D/g, ""));
+        const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+        const contentLength = end - start + 1;
+
+        const headers = {
+            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+            "Accept-Ranges": 'bytes',
+            "Content-Length": contentLength,
+            "Content-Type": "video/mp4"
+        };
+
+        res.writeHead(206, headers);
+
+        const videoStream = await axios.get(videoUrl, {
+            headers: {
+                Range: `bytes=${start}-${end}`,
+            },
+            responseType: 'stream',
+        });
+
+        videoStream.data.pipe(res);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).send("Internal Server Error");
     }
-    const videoPath = "./test.mp4";
-    const videoSize = fs.statSync(videoPath).size;
-    console.log("Size of the video is: ", videoSize);
-    const CHUNK_SIZE = 10**6; // 1 MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-    const contentLength = end - start + 1;
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": 'bytes',
-        "Content-Length": contentLength,
-        "Content-Type": "video/mp4"
-    }
-    res.writeHead(206, headers);
-    const videoStream = fs.createReadStream(videoPath, {start, end});
-    videoStream.pipe(res);
 });
- 
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
-})
+});
+
 
